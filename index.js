b8e5167c58dc713c69d181db10017a63136fcf81
jay-mehta531 <62137920+jay-mehta531@users.noreply.github.com>
2021-12-30 23:43:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,26 +24,3 @@ app.use("/api/project", projectRoute);
 app.listen(4000, () => {
   console.log("Server is up and running...");
 });
-
-// const fetchPosts = async () =>  {
-//   const options = {
-//     method:'POST',
-//     url: 'http://localhost:4000/api/user/login',
-//     data:{
-//     "email": "201701446@daiict.ac.in",
-//     "password": "Rc#26042000"
-// },
-//   }
-//   const res = await axios(options);
-//   console.log(res);
-//   }
-
-//   fetchPosts();
-// const fetchPosts = async () =>  {
-//   const res = await axios.get('http://localhost:4000/api/project/'  ,{headers:{
-//       'auth-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M…zNjN9.whh_Cfms6jPoB5n-Ov0j3LwH0dk68qBh7ltW3cKbeQs',
-//     }});
-// console.log(res);
-//   }
-
-//   fetchPosts();
